49e0eb2eb61a9ae549b2497b3c23dfa492846085
quang <pvaquang@gmail.com>
2023-07-19 22:54:17 +0700

    add new file

--- Counter.h
+++ Counter.h
@@ -1,1 +1,21 @@
 #pragma once
+
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <time.h>
+
+long long measureselectionSort(int a[], int n);
+long long measureInsertionSort(int a[], int n);
+long long measureBubbleSort(int a[], int n);
+long long measureShakerSort(int a[], int n);
+long long measureShellSort(int a[], int n);
+long long measureHeapSort(int a[], int n);
+long long measureMergeSort(int a[], int n);
+long long measureQuickSort(int a[], int n);
+long long measureCoutingSort(int a[], int n);
+long long measureRadixSort(int a[], int n);
+long long measureFlashSort(int a[], int n);
--- Sort.h
+++ Sort.h
@@ -1,3 +1,13 @@
 #pragma once
 
-void selectionSort()
+void selectionSort(int a[], int n, int& count_assign, int& count_compare);
+void insertionSort(int a[], int n, int& count_assign, int& count_compare);
+void bubbleSort(int a[], int n, int& count_assign, int& count_compare);
+void shakerSort(int a[], int n, int& count_assign, int& count_compare);
+void shellSort(int a[], int n, int& count_assign, int& count_compare);
+void heapSort(int a[], int n, int& count_assign, int& count_compare);
+void mergeSort(int a[], int n, int& count_assign, int& count_compare);
+void quickSort(int a[], int n, int& count_assign, int& count_compare);
+void countingSort(int a[], int n, int& count_assign, int& count_compare);
+void radixSort(int a[], int n, int& count_assign, int& count_compare);
+void flashSort(int a[], int n, int& count_assign, int& count_compare);
