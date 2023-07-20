aa006143a17014e32793edc66d32071f83f08239
quang <MY_NAME@example.com>
2023-07-20 09:46:32 +0700

    add prototype

--- main.cpp
+++ main.cpp
@@ -2,16 +2,10 @@
 
 void printArr(int* a, int n)
 {
-	for (size_t i = 0; i < n; i++)
-	{
-		cout << a[i] << " ";
-	}
+
 }
 int main(int argc, char* argv[])
 {
-    int a[] = {6, 5, 4, 3, 2, 1 };
-	long long cnt;
-	measureSelectionSort(a, 6, cnt);
-	cout << cnt;
+
     return 0;
 }
