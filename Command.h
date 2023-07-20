489568f4710087bf697b123d5e85dba30e399de7
quang <MY_NAME@example.com>
2023-07-20 16:35:12 +0700

    cmd 1

--- Command.h
+++ Command.h
@@ -4,12 +4,13 @@ double measureAlgorithm(char* algorithm, int* a, int n);
 bool checkValidAlgorithm(char* algorithm);
 int convertDataType(char* inputOrder);
 int convertDataTypeOutputParam(char* outputParam);
+int checkFile(char* inputFile);
 //measure number of count;
 void measureCount(char* algorithm, int* a, int n, long long& countCompare);
 
-void command1(char*, char*, char*);
+void command1(char* algorithm, char* inputFile, char* outputPara);
 
-void command2(char*, char*, char*, char*);
+void command2(char* algorithm, char* inputSize, char* order, char* outputPara);
 
 void command3(char*, char*, char*);
 
--- main.cpp
+++ main.cpp
@@ -5,11 +5,12 @@ int main(int argc, char* argv[])
 {
     char algorithm1[] = "counting-sort";
     char algorithm2[] = "radix-sort";
+    char outputPara[] = "-both";
     char hi[] = "input.txt";
     int n = 100000;
     int* a = new int[n];
-    generateData(n, a, 1);
+    generateData(n, a, 3);
     outputData(hi, a, n);
-    command4(algorithm1, algorithm2, hi);
+    command1(algorithm1, hi, outputPara);
     return 0;
 }
