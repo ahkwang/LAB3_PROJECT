8889b88d4fa542d2ba85aa6d643073eb411e3eb1
quang <MY_NAME@example.com>
2023-07-21 09:52:09 +0700

    add

--- main.cpp
+++ main.cpp
@@ -2,5 +2,10 @@
 
 int main(int argc, char** argv)
 {
+	char al[] = "flash-sort";
+	char input[] = "500000";
+	char order[] = "-rev";
+	char output[] = "-both";
+	command2(al, input, order, output);
 	return 0;
 }
